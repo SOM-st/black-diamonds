ee758cecb68d55acb30005a6b3e09c9eba53dbf4
Stefan Marr <git@stefan-marr.de>
2018-09-22 13:35:53 +0100

    Some minor style fixes
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/bd/basic/nodes/DummyParent.java
+++ src/bd/basic/nodes/DummyParent.java
@@ -1,5 +1,6 @@
 package bd.basic.nodes;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
@@ -24,6 +25,7 @@ public final class DummyParent extends RootNode {
 
   @Override
   public Object execute(final VirtualFrame frame) {
+    CompilerDirectives.transferToInterpreter();
     throw new UnsupportedOperationException(
         "This is a dummy node, and should not be executed");
   }
