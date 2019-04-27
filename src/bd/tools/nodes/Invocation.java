0f67b97e61a58f3d9b2bead2ccfaeb7daa5365ff
Stefan Marr <git@stefan-marr.de>
2019-04-27 21:01:25 +0100

    Added Invocation interface for tools
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/bd/tools/nodes/Invocation.java
@@ -0,0 +1,26 @@
+package bd.tools.nodes;
+
+import com.oracle.truffle.api.nodes.NodeInterface;
+
+
+/**
+ * Nodes implementing this interface represent some kind of invocation.
+ * Typically, this would be a method or function call, or some kind of message send.
+ *
+ * <p>
+ * The node implementing this interface should be the high-level node, that has a
+ * source section that is useful to represent the invocation in an editor.
+ *
+ * @param <Id> the type of the identifiers used for mapping to primitives, typically some form
+ *          of interned string construct
+ */
+public interface Invocation<Id> extends NodeInterface {
+
+  /**
+   * Return the identifier for the invocation that would be recognized by a developer,
+   * for instance when displayed in some tool.
+   *
+   * @return identifier of the invocation.
+   */
+  Id getInvocationIdentifier();
+}
