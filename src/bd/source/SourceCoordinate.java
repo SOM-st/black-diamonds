0e008d3d5159b6f7827896f35a39d8ace22237ec
Stefan Marr <git@stefan-marr.de>
2022-05-06 10:33:40 +0100

    Add SourceCoordinate.getLocationQualifier(int startIndex, Source source)
    This is a helper method, useful for use when a parser uses just a start index instead of a SourceCoordinate or SourceSection object.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/bd/source/SourceCoordinate.java
+++ src/bd/source/SourceCoordinate.java
@@ -111,6 +111,12 @@ public class SourceCoordinate {
     return ":" + section.getStartLine() + ":" + section.getStartColumn();
   }
 
+  public static String getLocationQualifier(final int startIndex, final Source source) {
+    int lineNumber = source.getLineNumber(startIndex);
+    int column = source.getColumnNumber(startIndex);
+    return ":" + lineNumber + ":" + column;
+  }
+
   public static String getURI(final Source source) {
     return source.getURI().toString();
   }
